c229115b67d37cb0c0a33c17893a2bac95e1da5f
sohamgohil123 <sohamgohil9@gmail.com>
2024-03-12 13:16:13 +0530

    add campaign details in patient details api and desease name in campaign list api

--- src/counsellor/services/counsellor.service.ts
+++ src/counsellor/services/counsellor.service.ts
@@ -14,38 +14,113 @@ export class CounsellorService {
         @InjectModel(CampaignAssign.name) private CampaignAssignModel: Model<CampaignAssign>,
     ) {}
 
-    async getPatientList(id: string, GetPatientListDto: GetPatientListDto) :Promise<any> {
+    async getPatientList(id: string, GetPatientListDto: GetPatientListDto): Promise<any> {
         const pageSize = GetPatientListDto.pageSize ?? 10;
         const page = GetPatientListDto?.page ?? 1;
-        const skip = pageSize * (page -1);
+        const skip = pageSize * (page - 1);
         const counsellorId = Types.ObjectId.createFromHexString(id);
-        let query = {counsellorId: counsellorId};
+        let query = { counsellorId: counsellorId };
 
         if (GetPatientListDto?.search) {
             const searchQueryString = GetPatientListDto.search.trim().split(" ").join("|");
-            query["$or"] = [{name: { $regex: `.*${searchQueryString}.*`, $options: "i" }},
-                            {city: { $regex: `.*${searchQueryString}.*`, $options: "i" }}]
+            query["$or"] = [{ name: { $regex: `.*${searchQueryString}.*`, $options: "i" } }, { city: { $regex: `.*${searchQueryString}.*`, $options: "i" } }];
         }
 
         const countProm = this.patientModel.count(query);
-        const dataProm = this.patientModel.find(query)
-            .skip(skip)
-            .limit(pageSize)
-            .exec();
+        const dataProm = this.patientModel.find(query).skip(skip).limit(pageSize).exec();
 
         const [count, list] = await Promise.all([countProm, dataProm]);
-        return {count, list};
+        return { count, list };
     }
 
     async getPatientDetails(id: string): Promise<any> {
-        const user = await this.patientModel.findById(id)
-        .populate("clinicId")
-        .populate("medicalCondition")
-        .populate("counsellorId", "firstName lastName email gender")
-        .exec();
+        // const user = await this.patientModel.findById(id)
+        // .populate("clinicId")
+        // .populate("medicalCondition")
+        // .populate("counsellorId", "firstName lastName email gender")
+        // .exec();
+        // const disease = await this.DiseaseDetailModel.findById(user.medicalCondition);
+        // const result = {
+        //     ...user.toObject(), // Convert Mongoose document to plain JavaScript object
+        //     medicalCondition: disease,
+        // };
+
+        const user: any = await this.patientModel.aggregate([
+            {
+                $match: { _id: Types.ObjectId.createFromHexString(id) },
+            },
+            {
+                $lookup: {
+                    from: "clinicsdetails",
+                    localField: "clinicId",
+                    foreignField: "_id",
+                    as: "clinicId",
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "counsellorId",
+                    foreignField: "_id",
+                    as: "counsellorId",
+                    pipeline: [
+                        {
+                            $project: {
+                                firstName: 1,
+                                lastName: 1,
+                                email: 1,
+                                gender: 1,
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                $lookup: {
+                    from: "campaignassigns",
+                    localField: "_id",
+                    foreignField: "userId",
+                    as: "campaignDetails",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "campaigns",
+                                localField: "campaignId",
+                                foreignField: "_id",
+                                as: "campaignId",
+                                pipeline: [
+                                    {
+                                        $project: {
+                                            name: 1,
+                                            weekData: 1,
+                                            createdAt: 1,
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                        {
+                            $unwind: "$campaignId",
+                        },
+                        {
+                            $project: {
+                                campaign: "$campaignId",
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                $unwind: "$clinicId",
+            },
+            {
+                $unwind: "$counsellorId",
+            },
+        ]);
+
         const disease = await this.DiseaseDetailModel.findById(user.medicalCondition);
         const result = {
-            ...user.toObject(), // Convert Mongoose document to plain JavaScript object
+            ...user[0], // Convert Mongoose document to plain JavaScript object
             medicalCondition: disease,
         };
 
@@ -53,29 +128,57 @@ export class CounsellorService {
     }
 
     async getCampaignList(id: string): Promise<any> {
-        const userId = Types.ObjectId.createFromHexString(id)
+        const userId = Types.ObjectId.createFromHexString(id);
         const result = await this.CampaignAssignModel.aggregate([
             {
-                $match: {userId: userId}
+                $match: { userId: userId },
             },
             {
                 $lookup: {
                     from: "campaigns",
                     localField: "campaignId",
                     foreignField: "_id",
-                    as: "campaignList"
-                }
+                    as: "campaignList",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "diseasedetails",
+                                localField: "diseaseId",
+                                foreignField: "_id",
+                                as: "diseaseList",
+                                pipeline: [
+                                    {
+                                        $project: {
+                                            diseaseName: 1,
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                        {
+                            $unwind: "$diseaseList",
+                        },
+                        {
+                            $project: {
+                                _id: 1,
+                                name: 1,
+                                diseaseName: "$diseaseList.diseaseName",
+                            },
+                        },
+                    ],
+                },
             },
             {
-                $unwind: "$campaignList"
+                $unwind: "$campaignList",
             },
             {
                 $project: {
                     _id: "$campaignList._id",
-                    name:"$campaignList.name"
-                }
-            }
-        ])
+                    name: "$campaignList.name",
+                    diseaseName: "$campaignList.diseaseName",
+                },
+            },
+        ]);
         return result;
     }
 }
