b0120fc1a2778a2fa9950ebbe30740eeadab6cf5
Shivam Gupta <shivam@Shivams-MacBook-Air.local>
2024-04-25 11:55:48 +0530

    add sub-admin

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -39,3 +39,37 @@ export class RegisterUserDto {
   @IsMongoId()
   otpVerificationCode: string;
 }
+
+
+export class RegisterSubAdminDto {
+  @IsEnum([AuthTypes.EMAIL, AuthTypes.MOBILE])
+  type: string;
+
+  @Length(2, 50)
+  firstName: string;
+
+  @IsOptional()
+  @Length(2, 50)
+  lastName: string;
+
+  @ValidateIf(req => req.type === AuthTypes.EMAIL)
+  @Transform(param => param.value.toLowerCase())
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
+
+  @ValidateIf(req => req.type === AuthTypes.MOBILE)
+  @Length(10)
+  mobile: string;
+
+  @IsOptional()
+  @IsString()
+  gender: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @IsOptional()
+  @IsString({ message: "Invalid value for role" })
+  role: string;
+}
--- src/auth/enums/role.enum.ts
+++ src/auth/enums/role.enum.ts
@@ -1,5 +1,6 @@
 export enum RoleType {
   ADMIN = "admin",
+  SUBADMIN="sub-admin",
   COUNSELLOR = "counsellor",
   USER = "user",
 }
