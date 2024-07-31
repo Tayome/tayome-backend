9861f3e560cc2e82c899393aec32bd8fd8046688
shivam  Gupta <shivamgupta8476@gmail.com>
2024-07-31 11:22:41 +0530

    fixes

--- src/clinic/services/clinics.service.ts
+++ src/clinic/services/clinics.service.ts
@@ -1,23 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable ,Logger} from "@nestjs/common";
 import { AddNewClinicDto } from "../dto/add-new-clinic.dto";
-import { InjectModel } from "@nestjs/mongoose";
+import { InjectConnection, InjectModel } from "@nestjs/mongoose";
 import { ClinicsDetail } from "../schemas/clinics-detail.schema";
 import { Model } from "mongoose";
 import { UploadService } from "src/utils/services/upload.service";
 import * as QRCode from "qrcode";
 import { ClinicsListDto } from "../dto/clinics-list.dto";
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class ClinicsService {
-    constructor(@InjectModel(ClinicsDetail.name) private ClinicDetailsModel: Model<ClinicsDetail>, private UploadService: UploadService) {}
+    private readonly logger = new Logger(ClinicsService.name);
+    constructor(@InjectModel(ClinicsDetail.name) private ClinicDetailsModel: Model<ClinicsDetail>, private UploadService: UploadService,@InjectConnection() private readonly connection: Connection,) {}
 
     async addClinic(addNewClinicDto: AddNewClinicDto): Promise<any> {
-        let addDetails = new this.ClinicDetailsModel(addNewClinicDto);
-        let response = await addDetails.save();
-        let qr = await this.generateQRCode(response);
-        response["qrCode"] = qr;
-        return await response.save();
-    }
+        const session = await this.connection.startSession();
+        session.startTransaction();
+        try {
+          let addDetails = new this.ClinicDetailsModel(addNewClinicDto);
+          let response = await addDetails.save({ session });
+    
+          let qr = await this.generateQRCode(response);  
+          
+          response["qrCode"] = qr;
+          await response.save({ session });
+    
+          await session.commitTransaction();
+          return response;
+        } catch (error) {
+          await session.abortTransaction();  
+          this.logger.error('Failed to add clinic and generate QR code', error.stack);
+          throw new Error('Failed to add clinic and generate QR code');
+        } finally {
+          session.endSession();
+        }
+      }
+
 
     async generateQRCode(clinicDetails: Object): Promise<any> {
         let text = "https://admin.tayome.net/app/form/" + clinicDetails["_id"];
